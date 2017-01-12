5b476623fc13ac2f4626ff857b382c15efa3365b
tedwei <tedwei@truetel.com>
2017-01-12 12:43:24 +0800

    example

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,50 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
+// import { AppRoutingModule }     from './app-routing.module';
 
 
 @NgModule({
-  imports:      [ 
+  imports: [
     BrowserModule,
-    FormsModule 
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
-  declarations: [ 
+  declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [ 
+    HeroService 
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
